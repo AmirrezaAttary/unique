91bbac9edeabfe77e14a15a3a64f7d696c3afa19
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2026-06-30 18:54:30 +0330

    update setting

--- core/core/settings.py
+++ core/core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d21r!)ccp=lnzx#acq_(3l9&_e9qt1sbya*len$bcw(vpfxjq5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["uniquelaboratory.org","www.uniquelaboratory.org"]
 
 
 # Application definition
@@ -118,12 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
-
-
-MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = "/home/hamisarv/public_html/media/"
+STATIC_ROOT = "/home/hamisarv/public_html/static/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "staticfiles",
