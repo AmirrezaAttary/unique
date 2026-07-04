71dc28ca8fd5bfb166f5df9eebd3b313c2eeb0e4
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2026-07-04 13:30:30 +0330

    Update views.py

--- core/app/shop/views.py
+++ core/app/shop/views.py
@@ -4,11 +4,8 @@ from .models import ProductModel, ProductCategoryModel
 
 def shop_index(request):
     products = list(ProductModel.objects.prefetch_related('category').all())
-    print("PRODUCTS COUNT:", len(products))
-    for p in products:
-        print(p.title, "-> categories:", list(p.category.all()))
-    
     categories = ProductCategoryModel.objects.all()
+
     products.sort(key=lambda p: p.category.first().title if p.category.exists() else '')
 
     context = {
