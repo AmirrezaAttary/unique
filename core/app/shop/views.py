64688a8b4433c10150d0abb4035e05791e1d283e
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2026-07-04 12:39:11 +0330

    Update views.py

--- core/app/shop/views.py
+++ core/app/shop/views.py
@@ -3,13 +3,21 @@ from .models import ProductModel, ProductCategoryModel
 
 
 def shop_index(request):
-    products = ProductModel.objects.prefetch_related('category').all()
-    categories = ProductCategoryModel.objects.all()
+    categories = ProductCategoryModel.objects.prefetch_related('productmodel_set').all()
+    
+    # ساخت لیستی از (کتگوری، محصولات آن کتگوری)
+    categorized_products = []
+    for category in categories:
+        products = category.productmodel_set.all()
+        if products.exists():
+            categorized_products.append({
+                'category': category,
+                'products': products,
+            })
 
     context = {
-        'products': products,
-        'categories': categories,
-        'products_count': products.count(),
+        'categorized_products': categorized_products,
+        'products_count': ProductModel.objects.count(),
         'categories_count': categories.count(),
     }
     return render(request, 'shop/index.html', context)
